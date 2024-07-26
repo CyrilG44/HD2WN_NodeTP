a3e007d15441d2dd777746044914f0e870d15a24
CyrilG44 <168413869+CyrilG44@users.noreply.github.com>
2024-07-26 14:31:43 +0200

    correction partie 5

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 //import express
 const express = require('express');
+
+//======================TOKEN=========================//
+//import jwt
+const jwt = require('jsonwebtoken');
+//clé secrète pour les token générés par l'application
+const JWT_SECRET = "TP_ENI";
+//==================================================//
+
 //import uuid version 4
 const {v4: uuidv4} = require('uuid');
 
@@ -26,7 +34,7 @@ mongoose.connect("mongodb://localhost:27017/db_tp_articles");
 // 2. les attributs attendus pour ce modèle
 // 3. le nom de la collection en base liée (synonyme de table en sql)
 const Article=mongoose.model('Article', {uid: String, title: String, content: String, author: String}, 'articles') 
-
+const User=mongoose.model('User', {pseudo: String,password: String}, 'users');
 //================================================//
 
 //démarrage
@@ -39,6 +47,11 @@ let articles = [
     { id: 2, title: 'Deuxième article', content: 'Contenu du deuxième article', author: 'Sanchez' },
     { id: 3, title: 'Troisième article', content: 'Contenu du troisième article', author: 'Toto' }
 ];*/
+/*
+let users = [
+    {pseudo: "cyril", password: "azerty"},
+    {pseudo: "admin", password: "Pa$$w0rd"}
+]*/
 
 //reponse json au format
 function responseService(response,code,message,data){
@@ -60,6 +73,28 @@ function surfaceControl(article){
     return errors;
 }
 
+// Middleware = fonction qui fera mur devant la route
+function authMiddleware(request,response,next){ //next signifie qu'on peut passer à la suite
+    //si token null ou erreur
+    if(request.headers.authorization == undefined || !request.headers.authorization){
+        return response.json({message: 'token null'});
+    }
+    // Extraire le token (qui est bearer)
+    const token = request.headers.authorization.substring(7);
+    // par defaut le result est null
+    let result = null;
+    // Si reussi à générer le token sans crash
+    try {
+        result = jwt.verify(token, JWT_SECRET);
+    } catch {
+    }
+    // Si result null donc token incorrect
+    if (!result) {
+        return response.json({ message: "token incorrect"});
+    }
+    // On passe le middleware
+    return next();
+}
 
 //routes
 /* si on veut externaliser la partie controller
@@ -80,7 +115,7 @@ app.get('/article/:id',async (request,response) => {
     return responseService(response,'702',`Impossible de récupérer un article ayant UID=${idParam}`,null);
 });
 
-app.post('/save-article',async (request,response) => {
+app.post('/save-article', authMiddleware, async (request,response) => {
     const articleJSON = request.body;
 
     let articleObject = null;
@@ -122,7 +157,7 @@ app.post('/save-article',async (request,response) => {
     return responseService(response,'200','Article ajouté avec succès',articleObject);
 });
 
-app.delete('/article/:id', async (request,response) => {
+app.delete('/article/:id', authMiddleware, async (request,response) => {//si plusieurs middleware alors injecter un tableau de middleware
     //recuperer l'id
     const idParam = request.params.id;
     //voir si l'article existe
@@ -134,5 +169,20 @@ app.delete('/article/:id', async (request,response) => {
     //sinon par défaut
     await articleObject.deleteOne();
     return responseService(response,'200',`article id=${idParam} supprimé avec succès`,articleObject);
+
+});
+
+app.post('/auth', async (request,response) => {
+    const userJSON = request.body;
+    const loggedUser = await User.findOne({pseudo:userJSON.pseudo, password:userJSON.password});//normalement il faut encrypter le mot de passe dans le serveur
+
+    //if user is not registered in db
+    if(!loggedUser){
+        return responseService(response,'404',`${userJSON.pseudo} ou ${userJSON.password} non valide(s)`,null); 
+    };
+
+    //else by default create a token
+    const token = jwt.sign({pseudo:loggedUser.pseudo}, JWT_SECRET,{expiresIn:'3h'});
+    return responseService(response,'202',"Authentifié(e) avec succès",token);
 
 });
